f7aaa6b5cb80d75dde0a989b624ae0881f61e445
Quasimodo <quasimodo1660@gmail.com>
2018-02-07 14:30:57 -0800

    First Node Demo

---
+++ app.js
@@ -0,0 +1,30 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        // console.log(request.headers['user-agent']);
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'}); 
+            console.log(contents); 
+            response.write('<p>'+request.headers['user-agent']+'</p>'); 
+            console.log(contents); 
+            response.end(); // finished!
+        });
+    }
+
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+        }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
